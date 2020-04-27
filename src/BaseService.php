f676f08cdea6b2dfea441c10baa95cae67700a1f
khan <amalsholihan@gmail.com>
2020-04-27 23:47:52 +0700

    validation di service

--- src/BaseService.php
+++ src/BaseService.php
@@ -28,5 +28,21 @@ class BaseService
 		{  
 			return call_user_func_array(array($load_service->repository, $method), $parameters);  
 		} 
-    }
+	}
+	
+	/**
+	 * [validate description]
+	 * @param  [type] $data     [description]
+	 * @param  array  $rules    [description]
+	 * @param  array  $messages [description]
+	 * @return [type]           [description]
+	 */
+	public function validate($data, $rules = [], $messages = [])
+	{
+		$rules = empty($rules) ? self::$rules : $rules;  
+		if(empty($rules)) return true;
+		$validator = Validator::make($data, $rules, $messages);		
+		if($validator->fails()) \KhanCode\LaravelBaseRest\Helpers::set_error($validator->errors()->toArray());
+		return true;
+	}
 }
